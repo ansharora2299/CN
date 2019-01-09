afe9415d32e414d156584309023181c22fa44967
ansh <ansharora2299@gmail.com>
2019-01-09 10:58:02 +0530

    bellmanford changed

--- 3.c
+++ 3.c
@@ -1,46 +1,47 @@
 #include<stdio.h>
-struct node
-{
-    unsigned dist[20];
-    unsigned from[20];
-}rt[10];
-int main()
-{
-    int costmat[20][20];
-    int nodes,i,j,k,count=0;
-    printf("\nEnter the number of nodes : ");
-    scanf("%d",&nodes);//Enter the nodes
-    printf("\nEnter the cost matrix :\n");
-    for(i=0;i<nodes;i++)
-    {
-        for(j=0;j<nodes;j++)
-        {
-            scanf("%d",&costmat[i][j]);
-            costmat[i][i]=0;
-            rt[i].dist[j]=costmat[i][j];
-            rt[i].from[j]=j;
-        }
-    }
-        do
-        {
-            count=0;
-            for(i=0;i<nodes;i++)
-            for(j=0;j<nodes;j++)
-            for(k=0;k<nodes;k++)
-                if(rt[i].dist[j]>costmat[i][k]+rt[k].dist[j])
-                {
-                    rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
-                    rt[i].from[j]=k;
-                    count++;
-                }
-        }while(count!=0);
-        for(i=0;i<nodes;i++)
-        {
-            printf("\n\n For router %d\n",i+1);
-            for(j=0;j<nodes;j++)
-            {
-                printf("\t\nnode %d via %d Distance %d",j+1,rt[i].from[j]+1,rt[i].dist[j]);
-            }
-        }
-    printf("\n\n");
+#include<stdlib.h>
+
+void main(){
+int mat[20][20],n,i,j,k,d[20],source;
+printf("Enter the number of nodes\n");
+scanf("%d",&n);
+ printf("Enter the cost matrix\n");
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+scanf("%d",&mat[i][j]);
+if(i!=j && mat[i][j]==0)
+mat[i][j]=999;
+}}
+
+for(i=0;i<n;i++)
+d[i]=999;
+
+int p[20]={-1};
+printf("source");
+scanf("%d",&source);
+
+d[source]=0;
+
+for(k=0;k<n;k++){
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+if(d[j]>d[i]+mat[i][j]){
+d[j]=d[i]+mat[i][j];
+p[j]=i;
+}
+}
+}
+}
+
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+if(d[j]>d[i]+mat[i][j]){
+printf("negative cycle exists");
+exit(0);
+}
+}}
+
+for(i=0;i<n;i++){
+printf("distance from %d to %d is %d\n",source,i,d[i]);
+}
 }
